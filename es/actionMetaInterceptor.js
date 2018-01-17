348afbd156da861e4c2af1025876758c7c1cf216
jony89 <ariel@emath.co.il>
2018-01-17 15:44:24 +0200

    remove uneeded output

--- es/actionMetaInterceptor.js
+++
@@ -1,3 +0,0 @@
-export default uniqueValue => action => (
-  { ...action, meta: { ...(action.meta || {}), uniqueValue } }
-);
--- es/reducerInterceptor.js
+++
@@ -1,5 +0,0 @@
-export default (interceptedReducer, uniqueValue) => (previousState = {}, action) => {
-  const actionUniqueValue = action.meta && action.meta.uniqueValue;
-  if (!actionUniqueValue) return interceptedReducer(previousState, action);
-  return actionUniqueValue !== uniqueValue ? previousState : interceptedReducer(previousState, action);
-};
